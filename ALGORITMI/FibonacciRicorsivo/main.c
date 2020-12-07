e332df350fc00027cbaea5856ae6dbe55a3d3276
raffaele <raffaeleventriglia6@gmail.com>
2020-12-07 14:59:27 +0100

    Aggiornamento

--- ALGORITMI/FibonacciRicorsivo/main.c
+++ ALGORITMI/FibonacciRicorsivo/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int fibonacci(int);
+double fib_ric_PD(int);
+double fib[100];
 
 int main(){
     int n;
+    for(int i = 0; i < 100; i++){
+        fib[i] = 0.0;
+    }
     printf("Inserire n: ");
     scanf("%d", &n);
-    printf("Fibonacci: %d", fibonacci(n));
+    printf("Fibonacci: %d\n", fibonacci(n));
+    printf("Fibonacci: %lf\n", fib_ric_PD(n));
     return 0;
 }
 
@@ -15,5 +21,17 @@ int fibonacci(int n){
         return n;
     } else {
         return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+double fib_ric_PD(int n){
+    if(n <= 1){
+        return (double)n;
+    }
+    if(fib[n] != 0.0){
+        return fib[n];
+    } else {
+        fib[n] = fib_ric_PD(n-1) + fib_ric_PD(n-2);
+        return fib[n];
     }
 }
