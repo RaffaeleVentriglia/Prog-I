3ec8b0c0f98bfdc6f99a2622ad94644f6291f79a
raffaele <raffaeleventriglia6@gmail.com>
2020-12-09 11:18:40 +0100

    Strlen_ricorsive

---
+++ ESERCITAZIONI/Strlen_ric/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int strlen_ric(char*);
+
+int main(){
+    char testo[100];
+    printf("Inserire il testo: ");
+    gets(testo);
+    printf("Lunghezza: %d\n", strlen_ric(testo));
+}
+
+int strlen_ric(char* testo){
+    if(*testo == '\0'){
+        return 0;
+    } else {
+        return 1 + strlen_ric(testo+1);
+    }
+}
